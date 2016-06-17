04a79246c1dc82d193793b1c74b988ed4f38d790
npcfotografia <nicolas_chiaradia@hotmail.com>
2016-06-17 18:43:30 -0300

    va de nuevo

--- StoredProcedures/DE_UNA.GetCompras.sql
+++ StoredProcedures/DE_UNA.GetCompras.sql
@@ -7,12 +7,27 @@ GO
 -- =============================================
 -- Description:	SP para recuperar las Compras de un Usuario.
 -- =============================================
-CREATE PROCEDURE [DE_UNA].GetCompras AS
+CREATE PROCEDURE [DE_UNA].GetCompras 
+	-- Parámetros para los filtros.
+	@cod_publi   numeric(18,0),
+	@cod_usuario decimal(20,0)
+AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
-	SELECT id_compra, cod_publi, cod_usuario, fecha_compra, monto, cantidad, cod_calificacion, estrellas, desc_calificacion
-	  FROM [DE_UNA].Compras;
+	SELECT id_compra
+	, cod_publi
+	, cod_usuario
+	, fecha_compra
+	, monto
+	, cantidad
+	, cod_calificacion
+	, estrellas
+	, desc_calificacion
+	FROM [DE_UNA].Compras
+	WHERE		(cod_publi    IN (@cod_publi)   OR @cod_publi	IS NULL)
+		  AND	(cod_usuario  IN (@cod_usuario) OR @cod_usuario IS NULL)
+	ORDER BY cod_publi
 END
--- StoredProcedures/DE_UNA.GetOfertas.sql
+++ StoredProcedures/DE_UNA.GetOfertas.sql
@@ -7,12 +7,19 @@ GO
 -- =============================================
 -- Description:	SP para recuperar las Ofertas de un Usuario.
 -- =============================================
-CREATE PROCEDURE [DE_UNA].GetOfertas AS
+CREATE PROCEDURE [DE_UNA].GetOfertas
+	-- Parámetros para los filtros.
+	@cod_publi   numeric(18,0),
+	@cod_usuario decimal(20,0)
+AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
 	SELECT id_oferta, cod_publi, cod_usuario, fecha_oferta, monto
-	  FROM [DE_UNA].Ofertas;
+
+	FROM [DE_UNA].Ofertas
+
+	WHERE (cod_publi    IN (@cod_publi)   OR @cod_publi IS NULL)
+	  AND (cod_usuario     IN (@cod_usuario) OR (@cod_usuario) IS NULL)
+	ORDER BY cod_publi
 END
