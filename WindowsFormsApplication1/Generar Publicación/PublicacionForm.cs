b3f3d1d566b045240bb702f577f1127c6aeeb50e
gerodriguez <germanrrodriguez@hotmail.com>
2016-06-20 00:02:44 -0300

    mergeando...

--- WindowsFormsApplication1/Generar Publicación/PublicacionForm.cs
+++ WindowsFormsApplication1/Generar Publicación/PublicacionForm.cs
@@ -14,39 +14,149 @@ namespace ME.UI
 {
     public partial class PublicacionForm : Form
     {
-        public PublicacionForm(Publicacion publicacion)
+        public PublicacionForm(Publicacion publicacion, bool modificable)
         {
-            //InitializeComponent();
+            InitializeComponent();
 
-            //if(publicacion.dni != null){
-            //    txtDescripción.Text = publicacion.nombre;
-            //    txtApellido.Text = publicacion.apellido;
-            //    txtNumeroDocC.Text = publicacion.dni;
-            //    ddlRol.SelectedValue = publicacion.cod_rol;
-            //    //txtTipoDocC.Text 
-            //    txtMailC.Text = publicacion.mail;
-            //    txtUserNameC.Text = publicacion.username;
-            //    //txtPasswordC.Text = publicacion.password;
-            //    txtTelefonoC.Text = publicacion.telefono;
-            //    txtDirCalleC.Text = publicacion.dir_calle;
-            //    txtDirNumeroC.Text = publicacion.dir_nro;
-            //    txtDirPisoC.Text = publicacion.dir_piso;
-            //    txtDirDeptoC.Text = publicacion.dir_depto;
-            //    txtDirLocalidadC.Text = publicacion.dir_localidad;
-            //    txtDirCPC.Text = publicacion.dir_cod_post;
-            //    dateTimeFechaInicio.Text = publicacion.fecha_nacimiento.ToString();
-            //}
+            if (publicacion == null)
+            {
+                this.Text = "Nueva Publicación";
+                txtDescripcion.Text = "";
+
+                cmbBoxTipoPubli.SelectedItem = null;
+                DTFechaInicio.Value = System.DateTime.Today; // Poner la fecha del archivo de configuración.
+                DTFechaVencimiento.Value = System.DateTime.Today; // Poner la fecha del archivo de configuración.
+                numStock.Value = 0;
+                numPrecio.Value = 0;
+                cmbBoxVisibilidad.SelectedItem = null;
+                cmbBoxEstado.SelectedItem = null;
+                cmbBoxRubro.SelectedItem = null;
+                cmbBoxEnvio.SelectedItem = null;
+                cmbBoxPreguntas.SelectedItem = null;
+                btnCancelar.Text = "Cancelar";
+
+                txtDescripcion.Enabled = true;
+                cmbBoxTipoPubli.Enabled = true;
+                DTFechaInicio.Enabled = true;
+                DTFechaVencimiento.Enabled = true;
+                numStock.Enabled = true;
+                numPrecio.Enabled = true;
+                cmbBoxVisibilidad.Enabled = true;
+                cmbBoxEstado.Enabled = true;
+                cmbBoxRubro.Enabled = true;
+                cmbBoxEnvio.Enabled = true;
+                cmbBoxPreguntas.Enabled = true;
+                btnGuardar.Enabled = true;
+                btnGuardar.Visible = true;
+
+            } else {
+                if (modificable) {
+                    this.Text = "Modificar Publicación" + publicacion.cod_publi.ToString();
+                    btnCancelar.Text = "Cancelar";
+
+                    txtDescripcion.Enabled = true;
+                    cmbBoxTipoPubli.Enabled = true;
+                    DTFechaInicio.Enabled = true;
+                    DTFechaVencimiento.Enabled = true;
+                    numStock.Enabled = true;
+                    numPrecio.Enabled = true;
+                    cmbBoxVisibilidad.Enabled = true;
+                    cmbBoxEstado.Enabled = true;
+                    cmbBoxRubro.Enabled = true;
+                    cmbBoxEnvio.Enabled = true;
+                    cmbBoxPreguntas.Enabled = true;
+                    btnGuardar.Enabled = true;
+                    btnGuardar.Visible = true;
+                }
+                else
+                {
+                    this.Text = "Publicación" + publicacion.cod_publi.ToString();
+                    btnCancelar.Text = "OK";
+
+                    txtDescripcion.Enabled = false;
+                    cmbBoxTipoPubli.Enabled = false;
+                    DTFechaInicio.Enabled = false;
+                    DTFechaVencimiento.Enabled = false;
+                    numStock.Enabled = false;
+                    numPrecio.Enabled = false;
+                    cmbBoxVisibilidad.Enabled = false;
+                    cmbBoxEstado.Enabled = false;
+                    cmbBoxRubro.Enabled = false;
+                    cmbBoxEnvio.Enabled = false;
+                    cmbBoxPreguntas.Enabled = false;
+                    btnGuardar.Enabled = false;
+                    btnGuardar.Visible = false;
+                }
+
+                txtDescripcion.Text = publicacion.descripcion;
+                cmbBoxTipoPubli.SelectedItem = publicacion.tipo_publi;
+                DTFechaInicio.Value = publicacion.fecha_inicio;
+                DTFechaVencimiento.Value = publicacion.fecha_vencimiento;
+                numStock.Value = publicacion.stock;
+                numPrecio.Value = publicacion.precio_producto;
+                cmbBoxVisibilidad.SelectedItem = publicacion.visibilidad;
+                cmbBoxEstado.SelectedItem = publicacion.estado;
+                cmbBoxRubro.SelectedItem = publicacion.rubro;
+                cmbBoxEnvio.SelectedItem = publicacion.con_envio;
+                cmbBoxPreguntas.SelectedItem = publicacion.con_preguntas;
+            }
+        }
+
+        private class VoF
+        {
+            public bool cod_valor { get; set; }
+            public string valor { get; set; }
+
+            public VoF(bool cod_valor, string valor)
+            {
+                this.cod_valor = cod_valor;
+                this.valor = valor;
+            }
         }
 
         private void PublicacionForm_Load(object sender, EventArgs e)
         {
-            //ddlRol.ValueMember = "cod_rol";
-            //ddlRol.DisplayMember = "nombre";
-            //ddlRol.DataSource = RolesHandler.GetRoles();
+            List<VoF> si_No = new List<VoF>();
+            VoF si = new VoF(true, "Si");
+            VoF no = new VoF(false, "No");    
+            si_No.Add(si);
+            si_No.Add(no);
 
-            //ddlRubro.ValueMember = "cod_rubro";
-            //ddlRubro.DisplayMember = "desc_larga";
-            //ddlRubro.DataSource = RubrosHandler.GetRubros();
+            this.numStock.Enabled = true;
+            this.DTFechaInicio.MinDate = System.DateTime.Today; // Poner la fecha del archivo de configuración.
+            this.DTFechaVencimiento.MinDate = System.DateTime.Today; // Poner la fecha del archivo de configuración.
+
+            this.cmbBoxTipoPubli.ValueMember   = "cod_tipo_publi";
+            this.cmbBoxTipoPubli.DisplayMember = "descripcion";
+            this.cmbBoxTipoPubli.DataSource = TipoPublicacionHandler.ListarTiposPublicacion();
+
+            this.cmbBoxVisibilidad.ValueMember   = "cod_visibilidad";
+            this.cmbBoxVisibilidad.DisplayMember = "descripcion";
+            this.cmbBoxVisibilidad.DataSource = VisibilidadHandler.ListarVisibilidades();
+
+            this.cmbBoxEstado.ValueMember   = "cod_estado";
+            this.cmbBoxEstado.DisplayMember = "descripcion";
+            this.cmbBoxEstado.DataSource = EstadoHandler.ListarEstados();
+
+            this.cmbBoxRubro.ValueMember   = "cod_rubro";
+            this.cmbBoxRubro.DisplayMember = "desc_larga";
+
+            bool esEmpresa = true; //SACAR. Esto representa a un usuario Empresa.
+
+            if (esEmpresa)
+            {
+                this.cmbBoxRubro.DataSource = RubroHandler.ObtenerRubro("Tecnología");
+            }else {
+                this.cmbBoxRubro.DataSource = RubroHandler.ListarRubros();
+            }
+
+            this.cmbBoxEnvio.ValueMember = "cod_valor";
+            this.cmbBoxEnvio.DisplayMember = "valor";
+            this.cmbBoxEnvio.DataSource = si_No;
+
+            this.cmbBoxPreguntas.ValueMember = "cod_valor";
+            this.cmbBoxPreguntas.DisplayMember = "valor";
+            this.cmbBoxPreguntas.DataSource = si_No;
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
@@ -54,32 +164,25 @@ namespace ME.UI
             this.Close();
         }
 
-        private void ddlRol_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            //if (ddlRol.SelectedValue.ToString() == "1")//Empresa
-            //{
-            //    pnlCliente.Visible = false;
-            //    pnlEmpresa.Visible = true;
-            //}
-            //else
-            //{
-            //    pnlEmpresa.Visible = false;
-            //    pnlCliente.Visible = true;
-            //}
-        }
-
         private void btnGuardar_Click(object sender, EventArgs e)
         {
-            //if (ddlRol.SelectedValue.ToString() == "1")//Empresa
-            //{
-            //    EmpresaHandler.Guardar(txtRazonSocial.Text, txtCiudadE.Text, txtNombreContacto.Text, ddlRubro.SelectedValue.ToString(), txtCUIT.Text, txtMailEmpresa.Text, txtUsernameE.Text, txtPasswordE.Text, true, txtTelefonoEmpresa.Text, txtDomCalleE.Text, txtDomNumeroE.Text, txtDomPisoE.Text, txtDomDeptoE.Text, txtLocalidadE.Text, txtDomCPE.Text);
-            //}
-            //else
-            //{
-            //    ClienteHandler.Guardar(txtDescripción.Text, txtApellido.Text, decimal.Parse(txtNumeroDocC.Text), txtPrecio.Text, txtMailC.Text, txtUserNameC.Text, txtPasswordC.Text, true, txtTelefonoC.Text, txtDirCalleC.Text, decimal.Parse(txtDirNumeroC.Text), decimal.Parse(txtDirPisoC.Text), txtDirDeptoC.Text, txtDirLocalidadC.Text, txtDirCPC.Text, DateTime.Parse(dateTimeFechaInicio.Text));
-            //}
+//            bool esEmpresa = true;   //SACAR. Esto representa a un usuario Empresa.
+
+            Publicacion nuevaPublicacion = PublicacionHandler.Guardar(txtDescripcion.Text, cmbBoxTipoPubli.SelectedIndex, DTFechaInicio.Value, DTFechaVencimiento.Value, numStock.Value, numPrecio.Value,
+                                       cmbBoxVisibilidad.SelectedIndex, cmbBoxEstado.SelectedIndex, cmbBoxRubro.SelectedIndex, cmbBoxEnvio.SelectedIndex, cmbBoxPreguntas.SelectedIndex);
+            
+            new PublicacionForm(nuevaPublicacion, false);
 
             this.Close();
+        }
+
+        private void cmbBoxTipoPubli_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbBoxTipoPubli.SelectedValue.ToString() == "Subasta")
+            {
+                numStock.Value = 1;
+                numStock.Enabled = false;
+            }
         }
     }
 }
