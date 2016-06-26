17416e389f0ea80741933a6732948f396bb39d29
npcfotografia <nicolas_chiaradia@hotmail.com>
2016-06-25 23:11:36 -0300

    ABM Visibilidades - incompleto

--- ME.Business/VisibilidadHandler.cs
+++ ME.Business/VisibilidadHandler.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace ME.Business
 {
@@ -17,6 +18,23 @@ namespace ME.Business
         public static List<Visibilidad> ListarVisibilidades()
         {
             return Visibilidad.GetVisibilidades();
+        }
+
+        public static int Nuevo(string descripcion, decimal costo_publicar, decimal porcentaje_venta, decimal costo_envio)
+        {
+            return Visibilidad.Nuevo(descripcion, costo_publicar, porcentaje_venta, costo_envio);
+        }
+
+        public static void Actualizar(decimal cod_visibilidad, string descripcion, decimal costo_publicar, decimal porcentaje_venta, decimal costo_envio)
+        {
+            Visibilidad.Actualizar(cod_visibilidad, descripcion, costo_publicar, porcentaje_venta, costo_envio);
+        }
+
+        public static void Eliminar(decimal cod_visibilidad)
+        {
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            parameters.Add(new SqlParameter("@cod_visibilidad", cod_visibilidad));
+            MEEntity.ExecuteSP("[DE_UNA].[EliminarVisibilidad]", parameters);
         }
     }
 }
--- ME.Data/Visibilidad.cs
+++ ME.Data/Visibilidad.cs
@@ -10,7 +10,7 @@ namespace ME.Data
 {
     public class Visibilidad
     {
-        public decimal   cod_visibilidad  { get; set; }
+        public decimal cod_visibilidad  { get; set; }
         public string  nombre           { get; set; }
         public decimal costo_publicar   { get; set; }
         public decimal porcentaje_venta { get; set; }
@@ -65,21 +65,82 @@ namespace ME.Data
                 connection.Open();
                 SqlDataReader reader = command.ExecuteReader();
 
-                while (reader.Read())
+                if (reader.Read())
                 {
-                    Visibilidad unaVisibilidad = new Visibilidad(
-                          decimal.Parse(reader["cod_visibilidad"].ToString())
-                        , reader["descripcion"].ToString()
-                        , decimal.Parse(reader["costo_publicar"].ToString())
-                        , decimal.Parse(reader["porcentaje_venta"].ToString())
-                        , decimal.Parse(reader["costo_envio"].ToString())
-                    );
+                    while (reader.Read())
+                    {
+                        Visibilidad unaVisibilidad = new Visibilidad(
+                              decimal.Parse(reader["cod_visibilidad"].ToString())
+                            , reader["descripcion"].ToString()
+                            , decimal.Parse(reader["costo_publicar"].ToString())
+                            , decimal.Parse(reader["porcentaje_venta"].ToString())
+                            , decimal.Parse(reader["costo_envio"].ToString())
+                            );
 
-                    visibilidadList.Add(unaVisibilidad);
+                        visibilidadList.Add(unaVisibilidad);
+                    }
+                    return visibilidadList;
                 }
+                else return null;
             }
 
-            return visibilidadList;
         }
+
+        public static int Nuevo(string descripcion, decimal costo_publicar, decimal porcentaje_venta, decimal costo_envio)
+        {
+            int result = 0;
+            using (SqlConnection connection = MEEntity.GetConnection())
+            {
+                SqlCommand command = new SqlCommand("[DE_UNA].[NuevaVisibilidad]", connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter param_descripcion = command.Parameters.AddWithValue("@descripcion", descripcion);
+                param_descripcion.SqlDbType = SqlDbType.NVarChar;
+
+                SqlParameter param_costo_publicar = command.Parameters.AddWithValue("@costo_publicar", costo_publicar);
+                param_costo_publicar.SqlDbType = SqlDbType.Decimal;
+
+                SqlParameter param_porcentaje_venta = command.Parameters.AddWithValue("@porcentaje_venta", porcentaje_venta);
+                param_porcentaje_venta.SqlDbType = SqlDbType.Decimal;
+
+                SqlParameter param_costo_envio = command.Parameters.AddWithValue("@costo_envio", costo_envio);
+                param_costo_envio.SqlDbType = SqlDbType.Decimal;
+
+
+                connection.Open();
+                command.ExecuteScalar();
+            }
+
+            return result;
+        }
+
+        public static void Actualizar(decimal cod_visibilidad, string descripcion, decimal costo_publicar, decimal porcentaje_venta, decimal costo_envio)
+        {
+            using (SqlConnection connection = MEEntity.GetConnection())
+            {
+                SqlCommand command = new SqlCommand("[DE_UNA].[ActualizarVisibilidad]", connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter param_cod_visibilidad = command.Parameters.AddWithValue("@cod_visibilidad", cod_visibilidad);
+                param_cod_visibilidad.SqlDbType = SqlDbType.Decimal;
+
+                SqlParameter param_descripcion = command.Parameters.AddWithValue("@descripcion", descripcion);
+                param_descripcion.SqlDbType = SqlDbType.NVarChar;
+
+                SqlParameter param_costo_publicar = command.Parameters.AddWithValue("@costo_publicar", costo_publicar);
+                param_costo_publicar.SqlDbType = SqlDbType.Decimal;
+
+                SqlParameter param_porcentaje_venta = command.Parameters.AddWithValue("@porcentaje_venta", porcentaje_venta);
+                param_porcentaje_venta.SqlDbType = SqlDbType.Decimal;
+
+                SqlParameter param_costo_envio = command.Parameters.AddWithValue("@costo_envio", costo_envio);
+                param_costo_envio.SqlDbType = SqlDbType.Decimal;
+
+
+                connection.Open();
+                command.ExecuteScalar();
+            }
+        }
+ 
     }
 }
