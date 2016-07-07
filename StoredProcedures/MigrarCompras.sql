3d6c562a95419aab6d1b6580e8861eae7cdeb277
npcfotografia <nicolas_chiaradia@hotmail.com>
2016-07-07 17:49:55 -0300

    bla

--- StoredProcedures/MigrarCompras.sql
+++ StoredProcedures/MigrarCompras.sql
@@ -15,6 +15,8 @@ BEGIN
 
 	PRINT '-------- Migrando Compras... --------';
 
+/*
+--  Este proc migra dos registros por compra, tal cual la tabla maestra, uno por la compra en sí y otro por la calificación de esa compra
 	INSERT INTO [DE_UNA].[Compras]
 		 SELECT M.[Publicacion_Cod]
 			  , C.[cod_usuario]
@@ -28,6 +30,45 @@ BEGIN
 			JOIN [DE_UNA].[Clientes] C ON M.[cli_dni] = C.[dni]
 		  WHERE [Compra_fecha] IS NOT NULL
 		  ORDER BY M.[Publicacion_Cod], C.[cod_usuario];
+*/
+
+-- Este procedimiento migra en dos partes, primero aquellos registros correspondientes a la compra calificada y luego aquellas compras no calificadas.
+	INSERT INTO [DE_UNA].[Compras]
+		 SELECT M.[Publicacion_Cod]
+			  , C.[cod_usuario]
+	 		  , M.[Compra_fecha]
+			  , M.[Publicacion_Precio]
+			  , M.[Compra_Cantidad]
+			  , M.[Calificacion_Codigo]
+			  , M.[Calificacion_Cant_Estrellas]
+			  , M.[Calificacion_Descripcion]
+		   FROM [gd_esquema].[Maestra] M
+			JOIN [DE_UNA].[Clientes] C ON M.[cli_dni] = C.[dni]
+		  WHERE [Compra_fecha] IS NOT NULL and M.Calificacion_Codigo is not null
+		  ORDER BY M.[Publicacion_Cod], C.[cod_usuario], M.[Calificacion_Codigo] desc;
+
+-- Aca me fijo si ya existe un registro en la tabla destino con los datos en común, para no agregarlo. O sea, inserta las compras no calificadas.
+	INSERT INTO [DE_UNA].[Compras]
+		 SELECT M.[Publicacion_Cod]
+			  , C.[cod_usuario]
+	 		  , M.[Compra_fecha]
+			  , M.[Publicacion_Precio]
+			  , M.[Compra_Cantidad]
+			  , M.[Calificacion_Codigo]
+			  , M.[Calificacion_Cant_Estrellas]
+			  , M.[Calificacion_Descripcion]
+		   FROM [gd_esquema].[Maestra] M
+			JOIN [DE_UNA].[Clientes] C ON M.[cli_dni] = C.[dni]
+		  WHERE [Compra_fecha] IS NOT NULL 	and M.Calificacion_Codigo is null 
+								and not exists (select 1 from [DE_UNA].[Compras] p where 
+															    p.cod_publi		= M.Publicacion_Cod
+															and p.cod_usuario   = C.cod_usuario
+															and p.cantidad		= M.Compra_Cantidad
+															and p.fecha_compra  = M.Compra_Fecha
+															and p.monto			= M.Publicacion_Precio
+												)
+		  ORDER BY M.[Publicacion_Cod], C.[cod_usuario], M.[Calificacion_Codigo] desc;
+
 
 	PRINT '----- Tabla de Compras, Migrada -----';
 
