1581e94fe595046c7e04366dc42083d29cc02ec9
npcfotografia <nicolas_chiaradia@hotmail.com>
2016-07-08 17:08:42 -0300

    migrar compras y reputaciones

--- StoredProcedures/MigrarCompras.sql
+++ StoredProcedures/MigrarCompras.sql
@@ -40,12 +40,43 @@ BEGIN
 			  , M.[Publicacion_Precio]
 			  , M.[Compra_Cantidad]
 			  , M.[Calificacion_Codigo]
-			  , M.[Calificacion_Cant_Estrellas]
+			  , round((M.[Calificacion_Cant_Estrellas] / 2),0)  -- la cantidad de estrellas adaptada al nuevo formato.
 			  , M.[Calificacion_Descripcion]
 		   FROM [gd_esquema].[Maestra] M
 			JOIN [DE_UNA].[Clientes] C ON M.[cli_dni] = C.[dni]
-		  WHERE [Compra_fecha] IS NOT NULL and M.Calificacion_Codigo is not null
+		  WHERE [Compra_fecha] IS NOT NULL and M.Calificacion_Codigo is not null and M.Calificacion_Cant_Estrellas is not null
 		  ORDER BY M.[Publicacion_Cod], C.[cod_usuario], M.[Calificacion_Codigo] desc;
+
+
+	-- Acá va el cursor que recorre la tabla Compras recién migrada y por cada registro suma la cantidad de estrellas 
+	-- en Usuarios.sum_calificacion e incrementa en uno Usuarios.cant_ventas donde 
+	-- Compras.cod_publi = Publicaciones.cod_publi y Publicaciones.cod_usuario = Usuarios.cod_usuario
+
+	DECLARE   @cod_publi			numeric(18,0)
+			, @estrellas			numeric(18,0);
+
+	DECLARE usuarios_cursor CURSOR FOR SELECT  C.cod_publi, c.estrellas FROM DE_UNA.Compras C
+
+	OPEN usuarios_cursor
+	FETCH NEXT FROM usuarios_cursor INTO @cod_publi, @estrellas
+
+	PRINT 'Comenzando Migracion de Calificaciones de Vendedores'
+
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+		update DE_UNA.Usuarios
+		set   sum_calificacion = sum_calificacion + @estrellas
+				, cant_ventas  = cant_ventas + 1
+			where DE_UNA.Usuarios.cod_usuario = (select P.cod_usuario from Publicaciones P where P.cod_publi = @cod_publi)
+		FETCH NEXT FROM usuarios_cursor INTO @cod_publi, @estrellas
+	END 
+
+	CLOSE usuarios_cursor;
+	DEALLOCATE usuarios_cursor;
+
+	PRINT '----- Calificaciones de Vendedores Migradas -----';
+
+
 
 -- Aca me fijo si ya existe un registro en la tabla destino con los datos en común, para no agregarlo. O sea, inserta las compras no calificadas.
 	INSERT INTO [DE_UNA].[Compras]
@@ -55,7 +86,7 @@ BEGIN
 			  , M.[Publicacion_Precio]
 			  , M.[Compra_Cantidad]
 			  , M.[Calificacion_Codigo]
-			  , M.[Calificacion_Cant_Estrellas]
+			  , (isnull(M.[Calificacion_Cant_Estrellas],0)/2)
 			  , M.[Calificacion_Descripcion]
 		   FROM [gd_esquema].[Maestra] M
 			JOIN [DE_UNA].[Clientes] C ON M.[cli_dni] = C.[dni]
@@ -68,8 +99,6 @@ BEGIN
 															and p.monto			= M.Publicacion_Precio
 												)
 		  ORDER BY M.[Publicacion_Cod], C.[cod_usuario], M.[Calificacion_Codigo] desc;
-
-
 	PRINT '----- Tabla de Compras, Migrada -----';
 
 END
